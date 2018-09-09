1d339913686a9d10bb051ac930d90417fe4df0d3
Jonathan Meyer <jmeyer@swri.org>
2018-09-09 16:27:41 -0500

    Something kind of works

--- descartes_light/src/descartes_light_node.cpp
+++ descartes_light/src/descartes_light_node.cpp
@@ -2,8 +2,29 @@
 #include <descartes_light/descartes_light.h>
 #include <opw_kinematics/opw_parameters_examples.h>
 
+#include <control_msgs/FollowJointTrajectoryAction.h>
+//#include <ros/console.h>
+#include <actionlib/client/simple_action_client.h>
+
 namespace
 {
+
+static bool executeTrajectory(const trajectory_msgs::JointTrajectory& trajectory)
+{
+  // Create a Follow Joint Trajectory Action Client
+  actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction> ac ("joint_trajectory_action", true);
+  if (!ac.waitForServer(ros::Duration(2.0)))
+  {
+    std::cerr << "Could not connect to action server";
+    return false;
+  }
+
+  control_msgs::FollowJointTrajectoryGoal goal;
+  goal.trajectory = trajectory;
+  goal.goal_time_tolerance = ros::Duration(1.0);
+
+  return ac.sendGoalAndWait(goal) == actionlib::SimpleClientGoalState::SUCCEEDED;
+}
 
 std::vector<descartes_light::PositionSamplerPtr> makePath()
 {
@@ -14,15 +35,15 @@ std::vector<descartes_light::PositionSamplerPtr> makePath()
                                                       Eigen::Isometry3d::Identity(),
                                                       Eigen::Isometry3d::Identity());
 
-  Eigen::Isometry3d reference = Eigen::Isometry3d::Identity() * Eigen::Translation3d(1.0, 0, 0.5) *
+  Eigen::Isometry3d reference = Eigen::Isometry3d::Identity() * Eigen::Translation3d(1.0, -1.0, 0.5) *
                                 Eigen::AngleAxisd(M_PI, Eigen::Vector3d::UnitY());
 
   std::vector<descartes_light::PositionSamplerPtr> result;
-  for (int i = 0; i < 5; ++i)
+  for (int i = 0; i < 10 * 2; ++i)
   {
     result.push_back(
           std::make_shared<descartes_light::CartesianPointSampler>(
-            reference * Eigen::Translation3d(i * .1, 0, 0), kin_interface));
+            reference * Eigen::Translation3d(0, i * 0.1, 0), kin_interface));
   }
 
   return result;
@@ -30,8 +51,9 @@ std::vector<descartes_light::PositionSamplerPtr> makePath()
 
 } // anon ns
 
-int main()
+int main(int argc, char** argv)
 {
+  ros::init(argc, argv, "test");
   // Define our vertex samplers
   const auto path = makePath();
 
@@ -52,6 +74,19 @@ int main()
     std::cerr << "Search for graph completion failed\n";
     return 1;
   }
+
+  // To joint trajectory
+  trajectory_msgs::JointTrajectory trajectory;
+  trajectory.joint_names = {"joint_1", "joint_2", "joint_3", "joint_4", "joint_5", "joint_6"};
+  for (std::size_t i = 0; i < solution.size() / 6; ++i)
+  {
+    trajectory_msgs::JointTrajectoryPoint pt;
+    pt.positions.assign(solution.begin() + i * 6, solution.begin() + (i + 1) * 6);
+    pt.time_from_start = ros::Duration(i * 0.5);
+    trajectory.points.push_back(pt);
+  }
+
+  executeTrajectory(trajectory);
 
   return 0;
 }
