1e1b2c4a1bd8285a599aded90b1b9be44f4f1aab
mripperger <michael.ripperger@swri.org>
2019-04-25 20:29:45 -0500

    Moved source files and changed to only instantiate template classes

---
+++ descartes_light/src/ladder_graph.cpp
@@ -0,0 +1,10 @@
+#include "descartes_light/impl/ladder_graph.hpp"
+
+namespace descartes_light
+{
+
+// Explicit template specialization
+template class LadderGraph<float>;
+template class LadderGraph<double>;
+
+} // namespace descartes_light
