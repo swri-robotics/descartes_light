708093dfee0d224849c225d0a64ac7f2b51cee8b
mripperger <michael.ripperger@swri.org>
2019-04-25 20:33:14 -0500

    Added collision interface implementation for tesseract in own package

---
+++ descartes_tesseract/include/descartes_tesseract/descartes_tesseract_collision_checker.h
@@ -0,0 +1,48 @@
+#ifndef DESCARTES_TESSERACT_TESSERACT_COLLISION_CHECKER_H
+#define DESCARTES_TESSERACT_TESSERACT_COLLISION_CHECKER_H
+
+#include <memory>
+#include <descartes_light/interface/collision_interface.h>
+#include <tesseract_core/basic_env.h>
+
+namespace descartes_light
+{
+
+template<typename FloatType>
+class TesseractCollision : public CollisionInterface<FloatType>
+{
+public:
+  /**
+   * @brief TesseractCollision
+   * @param collision_env The collision Environment
+   * @param active_links The list of active links
+   * @param joint_names The list of joint names in the order that the data will be provided to the validate function.
+   */
+  TesseractCollision(tesseract::BasicEnvConstPtr collision_env,
+                     const std::vector<std::string>& active_links,
+                     const std::vector<std::string>& joint_names);
+
+  bool validate(const FloatType* pos, const std::size_t size) override;
+
+  FloatType distance(const FloatType* pos, const std::size_t size) override;
+
+  typename CollisionInterface<FloatType>::Ptr clone() const override;
+
+  tesseract::BasicEnvConstPtr& environment() { return collision_env_; }
+  const tesseract::BasicEnvConstPtr& environment() const { return collision_env_; }
+
+private:
+  bool isContactAllowed(const std::string& a, const std::string& b) const;
+
+  tesseract::BasicEnvConstPtr collision_env_;
+  std::vector<std::string> active_link_names_;
+  std::vector<std::string> joint_names_;
+  tesseract::DiscreteContactManagerBasePtr contact_manager_;
+};
+
+using TesseractCollisionF = TesseractCollision<float>;
+using TesseractCollisionD = TesseractCollision<double>;
+
+} // namespace descartes_light
+
+#endif // DESCARTES_TESSERACT_TESSERACT_COLLISION_CHECKER_H
---
+++ descartes_tesseract/include/descartes_tesseract/impl/descartes_tesseract_collision_checker.hpp
@@ -0,0 +1,115 @@
+/*
+ * Software License Agreement (Apache License)
+ *
+ * Copyright (c) 2016, Southwest Research Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef DESCARTES_TESSERACT_IMPL_DESCARTES_TESSERACT_COLLISION_CHECKER_HPP
+#define DESCARTES_TESSERACT_IMPL_DESCARTES_TESSERACT_COLLISION_CHECKER_HPP
+
+#include <descartes_tesseract/descartes_tesseract_collision_checker.h>
+
+namespace descartes_light
+{
+
+template<typename FloatType>
+bool TesseractCollision<FloatType>::isContactAllowed(const std::string &a, const std::string &b) const
+{
+  return collision_env_->getAllowedCollisionMatrix()->isCollisionAllowed(a ,b);
+}
+
+template<typename FloatType>
+TesseractCollision<FloatType>::TesseractCollision(tesseract::BasicEnvConstPtr collision_env,
+                                                  const std::vector<std::string>& active_links,
+                                                  const std::vector<std::string>& joint_names)
+  : collision_env_(std::move(collision_env))
+  , active_link_names_(active_links)
+  , joint_names_(joint_names)
+  , contact_manager_(collision_env_->getDiscreteContactManager())
+{
+  contact_manager_->setActiveCollisionObjects(active_links);
+  contact_manager_->setIsContactAllowedFn(std::bind(&TesseractCollision<FloatType>::isContactAllowed, this, std::placeholders::_1,
+                                                    std::placeholders::_2));
+}
+
+template<typename FloatType>
+bool TesseractCollision<FloatType>::validate(const FloatType* pos,
+                                             const std::size_t size)
+{
+  // Happens in two phases:
+  // 1. Compute the transform of all objects
+  Eigen::Map<const Eigen::VectorXd> joint_angles(reinterpret_cast<const double*>(pos), long(size));
+  tesseract::EnvStatePtr env_state = collision_env_->getState(joint_names_, joint_angles);
+
+  // 2. Update the scene
+  contact_manager_->setCollisionObjectsTransform(env_state->transforms);
+
+  // 3. Ask the contact manager to go nuts
+  tesseract::ContactResultMap results;
+  contact_manager_->contactTest(results, tesseract::ContactTestType::FIRST);
+
+  // 4. Analyze results
+  const bool no_contacts = results.empty();
+
+//#ifndef NDEBUG
+//  for (const auto& contact : results)
+//  {
+//    std::cout << "Contact: " << contact.first.first << " - " << contact.first.second << " Distance: " << contact.second[0].distance << "\n";
+//  }
+//#endif
+
+  return no_contacts;
+}
+
+template<typename FloatType>
+FloatType TesseractCollision<FloatType>::distance(const FloatType* pos,
+                                                  const std::size_t size)
+{
+  // Happens in two phases:
+  // 1. Compute the transform of all objects
+  Eigen::Map<const Eigen::VectorXd> joint_angles(reinterpret_cast<const double*>(pos), long(size));
+  tesseract::EnvStatePtr env_state = collision_env_->getState(joint_names_, joint_angles);
+
+  // 2. Update the scene
+  contact_manager_->setCollisionObjectsTransform(env_state->transforms);
+
+  // 3. Ask the contact manager to go nuts
+  tesseract::ContactResultMap results;
+  contact_manager_->contactTest(results, tesseract::ContactTestType::CLOSEST);
+
+#ifndef NDEBUG
+  std::cout << "Called descartes_light::TesseractCollision::distance\n";
+  for (const auto& contact : results)
+  {
+    std::cout << "Contact: " << contact.first.first << " - " << contact.first.second << " Distance: " << contact.second[0].distance << "\n";
+  }
+#endif
+
+  if (results.empty())
+    return static_cast<FloatType>(contact_manager_->getContactDistanceThreshold());
+  else
+    return static_cast<FloatType>(results.begin()->second[0].distance);
+}
+
+template<typename FloatType>
+typename CollisionInterface<FloatType>::Ptr TesseractCollision<FloatType>::clone() const
+{
+  auto sptr = std::make_shared<TesseractCollision>(*this);
+  sptr->contact_manager_= this->contact_manager_->clone();
+  return sptr;
+}
+
+} // namespace descartes_light
+
+#endif // DESCARTES_TESSERACT_IMPL_DESCARTES_TESSERACT_COLLISION_CHECKER_HPP
