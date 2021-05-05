fa03c7b59729c98616cdeb6faac58a17b6377ea0
Michael Ripperger <michael.ripperger@swri.org>
2021-05-05 16:36:21 -0500

    Updated unit test to be generic for various implementations of the solver

--- descartes_light/test/utest.cpp
+++ descartes_light/test/utest.cpp
@@ -1,6 +1,6 @@
 #include <descartes_light/core/solver.h>
-#include <descartes_light/solvers/ladder_graph/ladder_graph_dag_search.h>
-#include <descartes_light/solvers/ladder_graph/ladder_graph.h>
+#include <descartes_light/edge_evaluators/euclidean_distance_edge_evaluator.h>
+#include <descartes_light/solvers/ladder_graph/ladder_graph_solver.h>
 
 #include <gtest/gtest.h>
 #include <functional>
@@ -9,12 +9,14 @@
 #include <numeric>
 #include <vector>
 
+using namespace descartes_light;
+
 static std::mt19937 RAND_GEN(0);
 
 template <typename FloatType>
 Eigen::Matrix<FloatType, Eigen::Dynamic, 1> generateRandomState(Eigen::Index dof)
 {
-  static std::normal_distribution<FloatType> dist;
+  std::normal_distribution<FloatType> dist;
   Eigen::Matrix<FloatType, Eigen::Dynamic, 1> sol(dof);
   for (Eigen::Index i = 0; i < dof; ++i)
     sol(i) = dist(RAND_GEN);
@@ -22,197 +24,177 @@ Eigen::Matrix<FloatType, Eigen::Dynamic, 1> generateRandomState(Eigen::Index dof
   return sol;
 }
 
+/**
+ * @brief Waypoint sampler that creates a set of random joint states with one all-zero joint state at a specified index
+ */
 template <typename FloatType>
-void addRandomVertices(descartes_light::LadderGraph<FloatType>& graph, const int max_vertices_per_rung)
+class RandomStateSampler : public WaypointSampler<FloatType>
 {
-  // Create a random number generator for determining how many vertices are in each rung
-  const int min_vertices_per_rung = std::max(1, max_vertices_per_rung / 2);
-  std::uniform_int_distribution<> vertex_count_dist(min_vertices_per_rung, max_vertices_per_rung);
-
-  // Create random rungs
-  for (std::size_t rung_idx = 0; rung_idx < graph.size(); ++rung_idx)
+public:
+  RandomStateSampler(const Eigen::Index dof, const std::size_t n_samples, const std::size_t zero_state_idx)
+    : dof_(dof), n_samples_(n_samples), zero_state_idx_(zero_state_idx)
   {
-    // Create a random number of vertices for this rung
-    std::size_t n_vertices = static_cast<std::size_t>(vertex_count_dist(RAND_GEN));
-
-    // Create random joint states for each vertex
-    std::vector<Eigen::VectorXd> sols(n_vertices);
-    auto& rung = graph.getRung(rung_idx);
-    for (std::size_t i = 0; i < n_vertices; ++i)
-      rung.nodes.push_back(
-          descartes_light::Node<FloatType>(generateRandomState<FloatType>(static_cast<Eigen::Index>(graph.dof())), 0));
   }
-}
 
-template <typename FloatType>
-void addRandomEdges(descartes_light::LadderGraph<FloatType>& graph)
-{
-  // Create random edges
-  for (std::size_t rung_idx = 0; rung_idx < graph.size() - 1; ++rung_idx)
+  virtual std::vector<Eigen::Matrix<FloatType, Eigen::Dynamic, 1>> sample() const override
   {
-    // Choose a random subset of vertices in the next rung to connect to
-    const std::size_t next_rung_size = graph.rungSize(rung_idx + 1);
-    std::uniform_int_distribution<std::size_t> edge_count_dist(1, next_rung_size);
-    std::normal_distribution<FloatType> cost_dist(10.0);
+    // Generate some random joint states
+    std::vector<Eigen::Matrix<FloatType, Eigen::Dynamic, 1>> waypoints;
+    waypoints.reserve(n_samples_);
+    std::generate_n(
+        std::back_inserter(waypoints), n_samples_, [this]() { return generateRandomState<FloatType>(dof_); });
 
-    auto& rung = graph.getRung(rung_idx);
-    std::vector<typename descartes_light::LadderGraph<FloatType>::EdgeList> edges_list;
-    for (std::size_t vertex_idx = 0; vertex_idx < rung.nodes.size(); ++vertex_idx)
+    // Set one of the joint states to all zeros
+    waypoints.at(zero_state_idx_) = Eigen::Matrix<FloatType, Eigen::Dynamic, 1>::Zero(this->dof_);
+
+    return waypoints;
+  }
+
+private:
+  const Eigen::Index dof_;
+  const std::size_t n_samples_;
+  const std::size_t zero_state_idx_;
+};
+
+/**
+ * @brief Edge evaluator class that evaluates all edges to be either valid or invalid, depending on the input, with a
+ * cost of zero
+ */
+template <typename FloatType>
+class NaiveEdgeEvaluator : public EdgeEvaluator<FloatType>
+{
+public:
+  NaiveEdgeEvaluator(const bool valid) : valid_(valid) {}
+
+  virtual std::pair<bool, FloatType> evaluate(const Eigen::Matrix<FloatType, Eigen::Dynamic, 1>&,
+                                              const Eigen::Matrix<FloatType, Eigen::Dynamic, 1>&) const override
+  {
+    return std::make_pair(valid_, 0.0);
+  }
+
+private:
+  const bool valid_;
+};
+
+/**
+ * @brief State evaluator that evaluates all states to be either valid or invalid, depending on the input, with a cost
+ * of zero
+ */
+template <typename FloatType>
+class NaiveStateEvaluator : public StateEvaluator<FloatType>
+{
+public:
+  NaiveStateEvaluator(const bool valid) : valid_(valid) {}
+
+  virtual std::pair<bool, FloatType> evaluate(const Eigen::Matrix<FloatType, Eigen::Dynamic, 1>&) const override
+  {
+    return std::make_pair(valid_, 0.0);
+  }
+
+private:
+  const bool valid_;
+};
+
+/**
+ * @brief Object for configuring a Descartes solver for the unit test fixture
+ */
+template <typename SolverT>
+struct SolverConfigurator
+{
+  using FloatType = typename SolverT::FloatT;
+  typename Solver<FloatType>::Ptr create();
+};
+
+// Ladder graph solver configurator
+template <typename FloatT>
+struct SolverConfigurator<LadderGraphSolver<FloatT>>
+{
+  using FloatType = FloatT;
+  typename Solver<FloatT>::Ptr create() { return std::make_unique<LadderGraphSolver<FloatT>>(6, 1); }
+};
+template struct SolverConfigurator<LadderGraphSolverF>;
+template struct SolverConfigurator<LadderGraphSolverD>;
+
+/**
+ * @brief Test fixture for the solver interface
+ */
+template <typename SolverConfiguratorT>
+class SolverFixture : public ::testing::Test
+{
+public:
+  using FloatType = typename SolverConfiguratorT::FloatType;
+
+  SolverFixture() : dof(6), n_waypoints(10), samples_per_waypoint(4)
+  {
+    samplers.reserve(n_waypoints);
+    zero_state_indices_.reserve(n_waypoints);
+
+    std::uniform_int_distribution<std::size_t> dist(0, samples_per_waypoint - 1);
+
+    // Create waypoint samplers with ra
+    for (std::size_t i = 0; i < n_waypoints; ++i)
     {
-      auto& node = rung.nodes[vertex_idx];
-
-      // Number of connections for this vertex
-      const std::size_t n_edges = edge_count_dist(RAND_GEN);
-
-      // Create a randomly shuffled list of vertex indices from the next rung
-      std::vector<unsigned> next_rung_vertex_idx(next_rung_size);
-      std::iota(next_rung_vertex_idx.begin(), next_rung_vertex_idx.end(), 0);
-      std::random_shuffle(next_rung_vertex_idx.begin(), next_rung_vertex_idx.end());
-
-      // Create the edges for this vertex
-      node.edges.resize(n_edges);
-      for (std::size_t i = 0; i < n_edges; ++i)
-      {
-        auto& edge = node.edges[i];
-        edge.cost = cost_dist(RAND_GEN);
-        edge.idx = next_rung_vertex_idx[i];
-      }
+      auto zero_state_idx = dist(RAND_GEN);
+      zero_state_indices_.push_back(zero_state_idx);
+      samplers.push_back(std::make_shared<RandomStateSampler<FloatType>>(dof, samples_per_waypoint, dist(RAND_GEN)));
     }
   }
+
+  SolverConfiguratorT configurator;
+  const Eigen::Index dof;
+  const std::size_t n_waypoints;
+  const std::size_t samples_per_waypoint;
+  std::vector<typename WaypointSampler<FloatType>::ConstPtr> samplers;
+  std::vector<std::size_t> zero_state_indices_;
+};
+
+using Implementations =
+    ::testing::Types<SolverConfigurator<LadderGraphSolverF>, SolverConfigurator<LadderGraphSolverD>>;
+
+TYPED_TEST_CASE(SolverFixture, Implementations);
+
+TYPED_TEST(SolverFixture, NoEdges)
+{
+  using FloatType = typename TypeParam::FloatType;
+  typename Solver<FloatType>::Ptr solver = this->configurator.create();
+
+  BuildStatus status = solver->build(this->samplers,
+                                     { std::make_shared<const NaiveEdgeEvaluator<FloatType>>(false) },
+                                     { std::make_shared<const NaiveStateEvaluator<FloatType>>(true) });
+  ASSERT_FALSE(status);
+
+  std::vector<std::size_t> expected_failed_edges(this->n_waypoints - 1);
+  std::iota(expected_failed_edges.begin(), expected_failed_edges.end(), 0);
+
+  ASSERT_TRUE(std::equal(status.failed_edges.begin(), status.failed_edges.end(), expected_failed_edges.begin()));
+  ASSERT_EQ(status.failed_vertices.size(), 0);
+
+  auto path = solver->search();
+  ASSERT_EQ(path.size(), 0);
 }
 
-template <typename FloatType>
-descartes_light::LadderGraph<FloatType> generateRandomGraph(const std::size_t dof,
-                                                            const std::size_t n_rungs,
-                                                            const int max_vertices_per_rung)
+TYPED_TEST(SolverFixture, KnownPathTest)
 {
-  descartes_light::LadderGraph<FloatType> graph(dof);
-  graph.resize(n_rungs);
+  using FloatType = typename TypeParam::FloatType;
+  typename Solver<FloatType>::Ptr solver = this->configurator.create();
 
-  addRandomVertices(graph, max_vertices_per_rung);
-  addRandomEdges(graph);
+  // Build a graph where one sample for each waypoint is an all zero state; evaluate edges using the Euclidean distance
+  // metric Since each waypoint has an all-zero state, the shortest path should be through these samples with a total
+  // cost of zero
+  BuildStatus status = solver->build(this->samplers,
+                                     { std::make_shared<const EuclideanDistanceEdgeEvaluator<FloatType>>() },
+                                     { std::make_shared<const NaiveStateEvaluator<FloatType>>(true) });
+  ASSERT_TRUE(status);
+  ASSERT_EQ(status.failed_vertices.size(), 0);
+  ASSERT_EQ(status.failed_edges.size(), 0);
 
-  return graph;
-}
+  auto path = solver->search();
+  ASSERT_EQ(path.size(), this->n_waypoints);
 
-template <typename FloatType>
-void runNoEdgesTest()
-{
-  descartes_light::LadderGraph<FloatType> graph(6);
-  graph.resize(10);
-  addRandomVertices<FloatType>(graph, 10);
-
-  descartes_light::DAGSearch<FloatType> search(graph);
-
-  FloatType total_cost = search.run();
-  ASSERT_NEAR(total_cost, std::numeric_limits<FloatType>::max(), 1.0e-6);
-}
-
-template <typename FloatType>
-void runCorrectGraphTest()
-{
-  const std::size_t dof = 6;
-  const std::size_t n_rungs = 10;
-  const int max_vertices_per_rung = 10;
-
-  descartes_light::LadderGraph<FloatType> graph = generateRandomGraph<FloatType>(dof, n_rungs, max_vertices_per_rung);
-  descartes_light::DAGSearch<FloatType> search(graph);
-  FloatType total_cost;
-  ASSERT_NO_THROW(total_cost = search.run());
-  EXPECT_GT(total_cost, 0.0);
-
-  std::cout << "Total cost: " << total_cost << std::endl;
-
-  std::vector<typename descartes_light::DAGSearch<FloatType>::predecessor_t> path;
-  ASSERT_NO_THROW(path = search.shortestPath());
-  EXPECT_EQ(path.size(), n_rungs);
-
-  std::cout << "Path:\n";
-  // Reconstruct the path cost
-  FloatType reconstructed_cost = 0.0;
-  for (std::size_t i = 0; i < path.size(); ++i)
+  for (const auto& state : path)
   {
-    const auto vertex_idx = path[i];
-    std::cout << vertex_idx << ", ";
-
-    if (i < path.size() - 1)
-    {
-      const auto next_idx = path[i + 1];
-      const auto& edge_list = graph.getRung(i).nodes[vertex_idx].edges;
-      auto it = std::find_if(edge_list.begin(),
-                             edge_list.end(),
-                             [next_idx](const descartes_light::Edge<FloatType>& edge) { return edge.idx == next_idx; });
-
-      reconstructed_cost += it->cost;
-    }
+    ASSERT_TRUE(state.isApprox(Eigen::Matrix<FloatType, Eigen::Dynamic, 1>::Zero(this->dof)));
   }
-  std::cout << std::endl;
-
-  ASSERT_TRUE(std::abs(total_cost - reconstructed_cost) < std::numeric_limits<FloatType>::epsilon());
-}
-
-template <typename FloatType>
-void runKnownPathTest()
-{
-  const std::size_t dof = 6;
-  const std::size_t n_rungs = 10;
-  const int max_vertices_per_rung = 10;
-
-  descartes_light::LadderGraph<FloatType> graph = generateRandomGraph<FloatType>(dof, n_rungs, max_vertices_per_rung);
-
-  std::vector<std::size_t> known_path;
-  known_path.reserve(graph.size());
-
-  // Choose a random start vertex and add it to the path
-  {
-    std::uniform_int_distribution<std::size_t> vertex_idx_dist(0, graph.getRung(0).nodes.size() - 1);
-    std::size_t vertex_idx = vertex_idx_dist(RAND_GEN);
-    known_path.push_back(vertex_idx);
-  }
-
-  for (std::size_t i = 0; i < graph.size() - 1; ++i)
-  {
-    // Get the current rung
-    auto& rung = graph.getRung(i);
-
-    // Choose a random edge from the selected vertex and set its cost to 0 so the search should choose it
-    auto& edges = rung.nodes[known_path[i]].edges;
-    std::uniform_int_distribution<std::size_t> edge_idx_dist(0, edges.size() - 1);
-    std::size_t edge_idx = edge_idx_dist(RAND_GEN);
-    auto& edge = edges[edge_idx];
-    edge.cost = 0.0;
-
-    // Add the vertex associated with this edge to the path
-    known_path.push_back(edge.idx);
-  }
-
-  descartes_light::DAGSearch<FloatType> search(graph);
-  FloatType total_cost = search.run();
-  EXPECT_NEAR(total_cost, 0.0, std::numeric_limits<FloatType>::epsilon());
-
-  auto path = search.shortestPath();
-  for (std::size_t i = 0; i < path.size(); ++i)
-  {
-    EXPECT_EQ(path.at(i), known_path.at(i));
-  }
-}
-
-TEST(DAGSearch, NoEdges)
-{
-  runNoEdgesTest<double>();
-  runNoEdgesTest<float>();
-}
-
-TEST(DAGSearch, CorrectGraph)
-{
-  runCorrectGraphTest<double>();
-  runCorrectGraphTest<float>();
-}
-
-TEST(DAGSearch, KnownPathTest)
-{
-  runKnownPathTest<double>();
-  runKnownPathTest<float>();
 }
 
 int main(int argc, char** argv)
