8394c7ba9112b5d339f0ddb5110d0e7a42ae3a50
Levi Armstrong <levi.armstrong@swri.org>
2021-06-10 09:58:11 -0500

    Add cmake linter

--- descartes_light/cmake/descartes_light_macros.cmake
+++ descartes_light/cmake/descartes_light_macros.cmake
@@ -1,34 +1,22 @@
 #
-# @file descartes_light_macros.cmake
-# @brief Common Tesseract CMake Macros
+# @file descartes_light_macros.cmake @brief Common Tesseract CMake Macros
 #
-# @author Levi Armstrong
-# @date October 15, 2019
-# @version TODO
-# @bug No known bugs
+# @author Levi Armstrong @date October 15, 2019 @version TODO @bug No known bugs
 #
 # @copyright Copyright (c) 2019, Southwest Research Institute
 #
-# @par License
-# Software License Agreement (Apache License)
-# @par
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-# http://www.apache.org/licenses/LICENSE-2.0
-# @par
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# @par License Software License Agreement (Apache License) @par Licensed under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0 @par Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing permissions and limitations under the License.
 
 macro(descartes_variables)
-  if (NOT DEFINED BUILD_SHARED_LIBS)
+  if(NOT DEFINED BUILD_SHARED_LIBS)
     set(BUILD_SHARED_LIBS ON)
   endif()
 
-  if (NOT DEFINED DESCARTES_ENABLE_CLANG_TIDY)
+  if(NOT DEFINED DESCARTES_ENABLE_CLANG_TIDY)
     set(DESCARTES_ENABLE_CLANG_TIDY OFF)
   endif()
 
@@ -38,28 +26,44 @@ macro(descartes_variables)
     set(DESCARTES_ENABLE_TESTING ON)
   endif()
 
-  if (NOT DEFINED DESCARTES_ENABLE_TESTING)
+  if(NOT DEFINED DESCARTES_ENABLE_TESTING)
     set(DESCARTES_ENABLE_TESTING OFF)
   endif()
 
-  if (NOT DEFINED DESCARTES_ENABLE_RUN_TESTING)
+  if(NOT DEFINED DESCARTES_ENABLE_RUN_TESTING)
     set(DESCARTES_ENABLE_RUN_TESTING OFF)
   endif()
 
-  if (DESCARTES_ENABLE_TESTING_ALL)
+  if(DESCARTES_ENABLE_TESTING_ALL)
     set(DESCARTES_ENABLE_TESTING ON)
     set(DESCARTES_ENABLE_CLANG_TIDY ON)
     set(DESCARTES_ENABLE_CODE_COVERAGE ON)
   endif()
 
   set(DESCARTES_COMPILE_DEFINITIONS "")
-  if (NOT DESCARTES_ENABLE_TESTING AND NOT DESCARTES_ENABLE_TESTING_ALL)
-    set(DESCARTES_CLANG_TIDY_ARGS "-line-filter=[{'name':'ikfast.h','lines':[[9999999,9999999]]}, {'name':'fanuc_m20ia10l_manipulator_ikfast_solver.cpp','lines':[[9999999,9999999]]}, {'name':'.h'}, {'name':'.hpp'}, {'name':'.cpp'}]" "-header-filter=.*" "-checks=-*,clang-analyzer-*,bugprone-*,cppcoreguidelines-avoid-goto,cppcoreguidelines-c-copy-assignment-signature,cppcoreguidelines-interfaces-global-init,cppcoreguidelines-narrowing-conversions,cppcoreguidelines-no-malloc,cppcoreguidelines-slicing,cppcoreguidelines-special-member-functions,misc-*,-misc-non-private-member-variables-in-classes,modernize-*,-modernize-use-trailing-return-type,-modernize-use-nodiscard,performance-*,readability-avoid-const-params-in-decls,readability-container-size-empty,readability-delete-null-pointer,readability-deleted-default,readability-else-after-return,readability-function-size,readability-identifier-naming,readability-inconsistent-declaration-parameter-name,readability-misleading-indentation,readability-misplaced-array-index,readability-non-const-parameter,readability-redundant-*,readability-simplify-*,readability-static-*,readability-string-compare,readability-uniqueptr-delete-release,readability-rary-objects")
+  if(NOT DESCARTES_ENABLE_TESTING AND NOT DESCARTES_ENABLE_TESTING_ALL)
+    set(DESCARTES_CLANG_TIDY_ARGS
+        "-line-filter=[{'name':'ikfast.h','lines':[[9999999,9999999]]}, {'name':'fanuc_m20ia10l_manipulator_ikfast_solver.cpp','lines':[[9999999,9999999]]}, {'name':'.h'}, {'name':'.hpp'}, {'name':'.cpp'}]"
+        "-header-filter=.*"
+        "-checks=-*,clang-analyzer-*,bugprone-*,cppcoreguidelines-avoid-goto,cppcoreguidelines-c-copy-assignment-signature,cppcoreguidelines-interfaces-global-init,cppcoreguidelines-narrowing-conversions,cppcoreguidelines-no-malloc,cppcoreguidelines-slicing,cppcoreguidelines-special-member-functions,misc-*,-misc-non-private-member-variables-in-classes,modernize-*,-modernize-use-trailing-return-type,-modernize-use-nodiscard,performance-*,readability-avoid-const-params-in-decls,readability-container-size-empty,readability-delete-null-pointer,readability-deleted-default,readability-else-after-return,readability-function-size,readability-identifier-naming,readability-inconsistent-declaration-parameter-name,readability-misleading-indentation,readability-misplaced-array-index,readability-non-const-parameter,readability-redundant-*,readability-simplify-*,readability-static-*,readability-string-compare,readability-uniqueptr-delete-release,readability-rary-objects"
+    )
 
     if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-      set(DESCARTES_COMPILE_OPTIONS -mno-avx -Wall -Wextra -Wconversion -Wsign-conversion -Wno-sign-compare)
+      set(DESCARTES_COMPILE_OPTIONS
+          -mno-avx
+          -Wall
+          -Wextra
+          -Wconversion
+          -Wsign-conversion
+          -Wno-sign-compare)
     elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-      set(DESCARTES_COMPILE_OPTIONS -mno-avx -Wall -Wextra -Winconsistent-missing-override -Wconversion -Wsign-conversion)
+      set(DESCARTES_COMPILE_OPTIONS
+          -mno-avx
+          -Wall
+          -Wextra
+          -Winconsistent-missing-override
+          -Wconversion
+          -Wsign-conversion)
     elseif(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
       set(DESCARTES_COMPILE_OPTIONS "")
       set(DESCARTES_COMPILE_DEFINITIONS _USE_MATH_DEFINES=ON)
@@ -67,11 +71,28 @@ macro(descartes_variables)
       message(WARNING "${CMAKE_CXX_COMPILER_ID} Unsupported compiler detected.")
     endif()
   else()
-    set(DESCARTES_CLANG_TIDY_ARGS "-line-filter=[{'name':'ikfast.h','lines':[[9999999,9999999]]}, {'name':'fanuc_m20ia10l_manipulator_ikfast_solver.cpp','lines':[[9999999,9999999]]}, {'name':'.h'}, {'name':'.hpp'}, {'name':'.cpp'}]" "-header-filter=.*" "-checks=-*,clang-analyzer-*,bugprone-*,cppcoreguidelines-avoid-goto,cppcoreguidelines-c-copy-assignment-signature,cppcoreguidelines-interfaces-global-init,cppcoreguidelines-narrowing-conversions,cppcoreguidelines-no-malloc,cppcoreguidelines-slicing,cppcoreguidelines-special-member-functions,misc-*,-misc-non-private-member-variables-in-classes,modernize-*,-modernize-use-trailing-return-type,-modernize-use-nodiscard,performance-*,readability-avoid-const-params-in-decls,readability-container-size-empty,readability-delete-null-pointer,readability-deleted-default,readability-else-after-return,readability-function-size,readability-identifier-naming,readability-inconsistent-declaration-parameter-name,readability-misleading-indentation,readability-misplaced-array-index,readability-non-const-parameter,readability-redundant-*,readability-simplify-*,readability-static-*,readability-string-compare,readability-uniqueptr-delete-release,readability-rary-objects" "-warnings-as-errors=-*,clang-analyzer-*,bugprone-*,cppcoreguidelines-avoid-goto,cppcoreguidelines-c-copy-assignment-signature,cppcoreguidelines-interfaces-global-init,cppcoreguidelines-narrowing-conversions,cppcoreguidelines-no-malloc,cppcoreguidelines-slicing,cppcoreguidelines-special-member-functions,misc-*,-misc-non-private-member-variables-in-classes,modernize-*,-modernize-use-trailing-return-type,-modernize-use-nodiscard,performance-*,readability-avoid-const-params-in-decls,readability-container-size-empty,readability-delete-null-pointer,readability-deleted-default,readability-else-after-return,readability-function-size,readability-identifier-naming,readability-inconsistent-declaration-parameter-name,readability-misleading-indentation,readability-misplaced-array-index,readability-non-const-parameter,readability-redundant-*,readability-simplify-*,readability-static-*,readability-string-compare,readability-uniqueptr-delete-release,readability-rary-objects")
+    set(DESCARTES_CLANG_TIDY_ARGS
+        "-line-filter=[{'name':'ikfast.h','lines':[[9999999,9999999]]}, {'name':'fanuc_m20ia10l_manipulator_ikfast_solver.cpp','lines':[[9999999,9999999]]}, {'name':'.h'}, {'name':'.hpp'}, {'name':'.cpp'}]"
+        "-header-filter=.*"
+        "-checks=-*,clang-analyzer-*,bugprone-*,cppcoreguidelines-avoid-goto,cppcoreguidelines-c-copy-assignment-signature,cppcoreguidelines-interfaces-global-init,cppcoreguidelines-narrowing-conversions,cppcoreguidelines-no-malloc,cppcoreguidelines-slicing,cppcoreguidelines-special-member-functions,misc-*,-misc-non-private-member-variables-in-classes,modernize-*,-modernize-use-trailing-return-type,-modernize-use-nodiscard,performance-*,readability-avoid-const-params-in-decls,readability-container-size-empty,readability-delete-null-pointer,readability-deleted-default,readability-else-after-return,readability-function-size,readability-identifier-naming,readability-inconsistent-declaration-parameter-name,readability-misleading-indentation,readability-misplaced-array-index,readability-non-const-parameter,readability-redundant-*,readability-simplify-*,readability-static-*,readability-string-compare,readability-uniqueptr-delete-release,readability-rary-objects"
+        "-warnings-as-errors=-*,clang-analyzer-*,bugprone-*,cppcoreguidelines-avoid-goto,cppcoreguidelines-c-copy-assignment-signature,cppcoreguidelines-interfaces-global-init,cppcoreguidelines-narrowing-conversions,cppcoreguidelines-no-malloc,cppcoreguidelines-slicing,cppcoreguidelines-special-member-functions,misc-*,-misc-non-private-member-variables-in-classes,modernize-*,-modernize-use-trailing-return-type,-modernize-use-nodiscard,performance-*,readability-avoid-const-params-in-decls,readability-container-size-empty,readability-delete-null-pointer,readability-deleted-default,readability-else-after-return,readability-function-size,readability-identifier-naming,readability-inconsistent-declaration-parameter-name,readability-misleading-indentation,readability-misplaced-array-index,readability-non-const-parameter,readability-redundant-*,readability-simplify-*,readability-static-*,readability-string-compare,readability-uniqueptr-delete-release,readability-rary-objects"
+    )
     if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-      set(DESCARTES_COMPILE_OPTIONS -mno-avx -Werror=all -Werror=extra -Werror=conversion -Werror=sign-conversion -Wno-sign-compare)
+      set(DESCARTES_COMPILE_OPTIONS
+          -mno-avx
+          -Werror=all
+          -Werror=extra
+          -Werror=conversion
+          -Werror=sign-conversion
+          -Wno-sign-compare)
     elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-      set(DESCARTES_COMPILE_OPTIONS -mno-avx -Werror=all -Werror=extra -Werror=inconsistent-missing-override -Werror=conversion -Werror=sign-conversion)
+      set(DESCARTES_COMPILE_OPTIONS
+          -mno-avx
+          -Werror=all
+          -Werror=extra
+          -Werror=inconsistent-missing-override
+          -Werror=conversion
+          -Werror=sign-conversion)
     elseif(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
       set(DESCARTES_COMPILE_OPTIONS "")
       set(DESCARTES_COMPILE_DEFINITIONS _USE_MATH_DEFINES=ON)
