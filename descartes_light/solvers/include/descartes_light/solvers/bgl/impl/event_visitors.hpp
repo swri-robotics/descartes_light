5aa94e283e7a84e7ad1dfe78748538f881ba87da
ctlewis <colin.lewis@swri.org>
2021-09-02 16:09:17 -0500

    PR changes

--- descartes_light/solvers/include/descartes_light/solvers/bgl/impl/event_visitors.hpp
+++ descartes_light/solvers/include/descartes_light/solvers/bgl/impl/event_visitors.hpp
@@ -15,11 +15,11 @@ namespace descartes_light
  * @details Throws the vertex descriptor that is the termination of the path once a vertex in the last rung of
  * the graph is operated on
  */
-template <typename FloatType>
-struct early_terminator : public boost::base_visitor<early_terminator<FloatType>>
+template <typename FloatType, typename EventType>
+struct early_terminator : public boost::base_visitor<early_terminator<FloatType, EventType>>
 {
   /** @brief Event filter typedef defining the events for which this visitor can be used */
-  typedef boost::on_examine_vertex event_filter;
+  typedef EventType event_filter;
 
   early_terminator(long last_rung_idx) : last_rung_idx_(last_rung_idx) {}
 
@@ -36,11 +36,11 @@ struct early_terminator : public boost::base_visitor<early_terminator<FloatType>
  * @brief Event visitor that adds all edges to each vertex dynamically as the vertex is discovered during the graph
  * search
  */
-template <typename FloatType>
-struct add_all_edges_dynamically : public boost::base_visitor<add_all_edges_dynamically<FloatType>>
+template <typename FloatType, typename EventType>
+struct add_all_edges_dynamically : public boost::base_visitor<add_all_edges_dynamically<FloatType, EventType>>
 {
   /** @brief Event filter typedef defining the events for which this visitor can be used */
-  typedef boost::on_discover_vertex event_filter;
+  typedef EventType event_filter;
 
   add_all_edges_dynamically(std::vector<typename EdgeEvaluator<FloatType>::ConstPtr>& edge_eval,
                             std::vector<std::vector<VertexDesc<FloatType>>>& ladder_rungs)
@@ -58,19 +58,18 @@ struct add_all_edges_dynamically : public boost::base_visitor<add_all_edges_dyna
       std::size_t next_rung = static_cast<std::size_t>(current_rung + 1);
       if (next_rung < ladder_rungs_.size())
       {
-        FloatType cost;
         for (std::size_t s = 0; s < ladder_rungs_[next_rung].size(); ++s)
         {
           std::pair<bool, FloatType> results = eval_[static_cast<size_t>(current_rung)]->evaluate(
               *g[u].sample.state, *g[ladder_rungs_[next_rung][s]].sample.state);
           if (results.first)
           {
-            cost = results.second + g[ladder_rungs_[next_rung][s]].sample.cost;
+            FloatType cost = results.second + g[ladder_rungs_[next_rung][s]].sample.cost;
             if (current_rung == 0)
               cost += g[u].sample.cost;
-            VertexDesc<FloatType> sap = ladder_rungs_[next_rung][s];
+            VertexDesc<FloatType> target_vert = ladder_rungs_[next_rung][s];
             BGLGraph<FloatType>* mutable_graph_ = const_cast<BGLGraph<FloatType>*>(&g);
-            boost::add_edge(u, sap, cost, *mutable_graph_);
+            boost::add_edge(u, target_vert, cost, *mutable_graph_);
           }
         }
       }
@@ -78,7 +77,7 @@ struct add_all_edges_dynamically : public boost::base_visitor<add_all_edges_dyna
   }
 
   std::vector<typename EdgeEvaluator<FloatType>::ConstPtr> eval_;
-  std::vector<std::vector<VertexDesc<FloatType>>>& ladder_rungs_;
+  std::vector<std::vector<VertexDesc<FloatType>>> ladder_rungs_;
 };
 
 /**
