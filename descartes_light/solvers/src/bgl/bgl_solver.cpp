3555e24f212b88baae3c506e506bc511acb3f760
Michael Ripperger <michael.ripperger@swri.org>
2021-09-03 16:31:15 -0500

    Revised explicit template instantiations of BGL solver classes

--- descartes_light/solvers/src/bgl/bgl_solver.cpp
+++ descartes_light/solvers/src/bgl/bgl_solver.cpp
@@ -19,31 +19,48 @@
 #include <descartes_light/solvers/bgl/impl/bgl_dijkstra_solver.hpp>
 #include <descartes_light/solvers/bgl/impl/utils.hpp>
 
+#include <descartes_light/descartes_macros.h>
+DESCARTES_IGNORE_WARNINGS_PUSH
+#include <boost/graph/visitors.hpp>
+DESCARTES_IGNORE_WARNINGS_POP
+
 namespace descartes_light
 {
+// Event visitors
+template struct early_terminator<double, boost::on_examine_vertex>;
+template struct early_terminator<float, boost::on_examine_vertex>;
+
+template struct add_all_edges_dynamically<float, boost::on_examine_vertex>;
+template struct add_all_edges_dynamically<double, boost::on_examine_vertex>;
+
 // Explicit template instantiation
 // Partial implementations
-template class BGLSolverBase<double>;
-template class BGLSolverBase<float>;
+template class BGLSolverBase<double, early_terminator<double, boost::on_examine_vertex>>;
+template class BGLSolverBase<float, early_terminator<float, boost::on_examine_vertex>>;
+template class BGLSolverBase<double, boost::null_visitor>;
+template class BGLSolverBase<float, boost::null_visitor>;
 
-template class BGLSolverBaseSVDE<double>;
-template class BGLSolverBaseSVDE<float>;
+template class BGLSolverBaseSVDE<double, early_terminator<double, boost::on_examine_vertex>>;
+template class BGLSolverBaseSVDE<float, early_terminator<float, boost::on_examine_vertex>>;
+template class BGLSolverBaseSVDE<double, boost::null_visitor>;
+template class BGLSolverBaseSVDE<float, boost::null_visitor>;
 
-template class BGLSolverBaseSVSE<double>;
-template class BGLSolverBaseSVSE<float>;
+template class BGLSolverBaseSVSE<double, early_terminator<double, boost::on_examine_vertex>>;
+template class BGLSolverBaseSVSE<float, early_terminator<float, boost::on_examine_vertex>>;
+template class BGLSolverBaseSVSE<double, boost::null_visitor>;
+template class BGLSolverBaseSVSE<float, boost::null_visitor>;
 
 // Full Implementations
-template class BGLDijkstraSVSESolver<double>;
-template class BGLDijkstraSVSESolver<float>;
+template class BGLDijkstraSVDESolver<double, early_terminator<double, boost::on_examine_vertex>>;
+template class BGLDijkstraSVDESolver<float, early_terminator<float, boost::on_examine_vertex>>;
+template class BGLDijkstraSVDESolver<double, boost::null_visitor>;
+template class BGLDijkstraSVDESolver<float, boost::null_visitor>;
 
-template class BGLEfficientDijkstraSVSESolver<double>;
-template class BGLEfficientDijkstraSVSESolver<float>;
+template class BGLDijkstraSVSESolver<double, early_terminator<double, boost::on_examine_vertex>>;
+template class BGLDijkstraSVSESolver<float, early_terminator<float, boost::on_examine_vertex>>;
+template class BGLDijkstraSVSESolver<double, boost::null_visitor>;
+template class BGLDijkstraSVSESolver<float, boost::null_visitor>;
 
-template class BGLDijkstraSVDESolver<double>;
-template class BGLDijkstraSVDESolver<float>;
-
-template class BGLEfficientDijkstraSVDESolver<double>;
-template class BGLEfficientDijkstraSVDESolver<float>;
 // Free functions
 template SubGraph<double> createDecoratedSubGraph(const BGLGraph<double>& g);
 template SubGraph<float> createDecoratedSubGraph(const BGLGraph<float>& g);
